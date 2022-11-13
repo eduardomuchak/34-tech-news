974b7344dd8c60349adba5c781874a8040448c96
Eduardo Muchak <eduardomuchak@gmail.com>
2022-11-13 18:39:58 -0300

    feat: :sparkles: funcao top_5_categories finalizada
    requisito 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -16,4 +16,20 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    categories = [new["category"] for new in news_list]
+    categories_count = {}
+
+    for category in categories:
+        if category in categories_count:
+            categories_count[category] += 1
+        else:
+            categories_count[category] = 1
+
+    categories_count_sorted = sorted(
+        categories_count,
+        key=lambda category: (-categories_count[category], category),
+    )
+
+    top_5 = categories_count_sorted[:5]
+    return top_5
