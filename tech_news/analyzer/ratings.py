fbc0fab03b0f6dffa6345a19240d319d4a589e74
Eduardo Muchak <eduardomuchak@gmail.com>
2022-11-13 18:31:38 -0300

    feat: :sparkles: funcao top_5_news finalizada
    requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    news_list = sorted(
+        news_list,
+        key=lambda new: new["comments_count"],
+        reverse=True,
+    )
+
+    top_5 = [(new["title"], new["url"]) for new in news_list[:5]]
+    return top_5
 
 
 # Requisito 11
