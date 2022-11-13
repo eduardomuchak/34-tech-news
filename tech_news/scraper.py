29d5e25dd543aa3accf9b1d69be6baf7aa47466e
Eduardo Muchak <eduardomuchak@gmail.com>
2022-11-13 17:05:35 -0300

    feat: :sparkles: funcao scrape_novidades finalizada

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,7 +18,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    titles = selector.css("a.cs-overlay-link::attr(href)").getall()
+    return titles
 
 
 # Requisito 3
